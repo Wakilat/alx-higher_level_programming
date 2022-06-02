0edd8c02adea9d4a51e3f71c7ee0a12808eb2b1b
Wakilat <wakilatadeola@gmail.com>
2022-06-02 06:13:44 -0700

    importing modules with python

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def add(a, b):
-    return(a + b)
+
+    """My addition function
+        Args:
+            a: first integer
+            b: second integer
+        Returns:
+            The return value is a + b
+    """
+    return (a + b)
