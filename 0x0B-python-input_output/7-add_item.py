363894fbf3b5b743dbd811950e6a991fa4f42f38
Wakilat <wakilatadeola@gmail.com>
2022-07-05 08:43:11 -0700

    correction

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
 """
-script that adds all arguments to a Python list, and then save them to a file
+Module for taking arguments and adds to a list, saves to file
 """
-import json
+
+
 import sys
-
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-try:
-    py_list = load_from_json_file(filename)
-except FileNotFoundError:
-    py_list = []
-finally:
-    for el in sys.argv[1:]:
-        py_list.append(str(el))
-    save_to_json_file(py_list, filename)
+
+def main():
+    """contain main code in function
+    """
+    try:
+        new_list = load_from_json_file('add_item.json')
+    except:  # file didn't exist to read from
+        new_list = []
+
+    new_list.extend([sys.argv[i] for i in range(0, len(sys.argv)) if i != 0])
+    try:
+        save_to_json_file(new_list, 'add_item.json')
+    except:
+        pass
+
+main()
